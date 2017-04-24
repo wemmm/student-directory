ceef9453653c7c015d50019fc5e01f5ca6c629df
wemmm <jenny.wem@gmail.com>
2017-04-24 10:46:01 +0100

    Refactored directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +13,4 @@ puts "Freddy Kreuger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-print "Overall, we have "
-print 11
-puts " great students"
+print "Overall, we have #{student_count} great students."
