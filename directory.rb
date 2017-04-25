d4c9daa40f12f9268debd1dfa800cff0e4ca1e49
wemmm <jenny.wem@gmail.com>
2017-04-25 10:49:06 +0100

    Modified directory.rb to accept user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,16 @@
 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Kreuger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def input_students
+  puts "Please enter the names of the students."
+  puts "To finish, just hit return twice!"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students!"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The Students of Villains Academy"
@@ -28,6 +27,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students."
 end
 
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
