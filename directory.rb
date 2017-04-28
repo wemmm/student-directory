99875469fa04eff7a8a566413df9400a1ff408c3
wemmm <jenny.wem@gmail.com>
2017-04-28 12:56:19 +0100

    directory.rb can now save student info to a .csv file

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
   puts "1. Input the students."
   puts "2. Show the students."
+  puts "3. Save the list of students."
   puts "9. Exit."
 end
 
@@ -19,8 +20,10 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
-    Exit
+    exit
   else
     puts "I don't know what you mean! Please try again."
   end
@@ -57,6 +60,16 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students."
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(", ")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
